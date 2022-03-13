40c801d2774cc2c1a9da1083a57e2e3253d448fd
DarkVypr <70484211+DarkVypr@users.noreply.github.com>
2022-03-13 16:29:59 +0000

    followage address's you correctly

--- commands/fa.ts
+++ commands/fa.ts
@@ -6,8 +6,8 @@ module.exports = async (client, context) => {
       const followData = await utils.fetch(`https://api.ivr.fi/twitch/subage/${user}/${channel}`)
       user = followData.username
       channel = followData.channel
-      const address = user.toLowerCase() == context.user ? { pronoun: 'You', determiner: 'Your', name: `You`, verb: 'have', verb2: 'are', idiom: "aren't" } : { pronoun: 'They', determiner: 'Their', name: `@${user}`, verb: 'has', verb2: 'is', idiom: "isn't" }
-      if (!followData.followedAt) { return { success: true, reply: `${address.name} ${address.verb2} not following @${channel}.` } }
+      const address = user.toLowerCase() == context.user ? { name: `You`, verb: 'are' } : { name: `@${user}`, verb: 'is' }
+      if (!followData.followedAt) { return { success: true, reply: `${address.name} ${address.verb} not following @${channel}.` } }
       const [followDelta, followDate] = [utils.formatDelta(followData.followedAt), utils.formatDate(followData.followedAt, "fullDate")]
       return { success: true, reply: `${address.name} followed @${channel} on ${followDate} which was ${followDelta} ago.` }
     }catch(e) {
