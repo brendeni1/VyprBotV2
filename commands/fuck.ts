ac74ea5a209d94b22fa39cc6e8ae3082617db2f8
DarkVypr <70484211+DarkVypr@users.noreply.github.com>
2022-03-27 00:49:25 +0000

    tuck command and best emotes for other similar commands

--- commands/fuck.ts
+++ commands/fuck.ts
@@ -4,6 +4,6 @@ module.exports = async (client, context) => {
   if(!context.args[0]) { return { success: false, reply: `Provide a target.` } }
   const target = context.args[0].replace('@', '')
   context.args.shift()
-  const message = context.args[0] ? context.args.join(' ') : 'gachiGASM'
+  const message = context.args[0] ? context.args.join(' ') : await utils.bestEmote(context.channel, ['gachiBASS', 'gachiGASM', 'gachiHYPER', 'gachiHyper', 'Kreygasm', 'peepoShy', '🍆', '😫'])
   return { success: true, reply: `${context.display} fucks ${target} in the ass: ${message}` }
 }
---
+++ commands/tuck.ts
@@ -0,0 +1,9 @@
+import utils from '../utils'
+
+module.exports = async (client, context) => {
+  if(!context.args[0]) { return { success: false, reply: `Provide a target.` } }
+  const target = context.args[0].replace('@', '')
+  context.args.shift()
+  const message = context.args[0] ? context.args.join(' ') : await utils.bestEmote(context.channel, ['catKISS', 'KissAWeeb', 'FumoKiss', 'FumoTuck', 'SoCute', 'HUGGIES', 'Tuckacutie', 'CatAHomie', '🛌', '😴']) + ' 💝'
+  return { success: true, reply: `${context.display} tucks ${target} into bed: ${message}` }
+}
