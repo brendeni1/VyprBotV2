daa65d9548ece7a0aa09a3ed8e97af4e0fe1f098
Brenden <70484211+DarkVypr@users.noreply.github.com>
2022-04-17 13:57:55 +0000

    query alias for wolfram

--- commands/code.ts
+++ commands/code.ts
@@ -14,6 +14,7 @@ module.exports = async (client, context) => {
   if(command == 'birthday') { command = 'bday' }
   if(command == 'top') { command = 'topstreams' }
   if(command == 'subage') { command = 'sa' }
+	if (command == 'query') { command = 'wolfram' }
 	let commandCheck = (await fs.readdir(__dirname)).indexOf(command)
 	if (commandCheck == -1 || command == 'handler.js' || command == 'template.js') {
 		return {
--- commands/handler.ts
+++ commands/handler.ts
@@ -4,24 +4,12 @@ import cooldown from '../cooldown'
 import utils from '../utils'
 
 module.exports = async (command, client, context) => {
-	if (command == 'followage') {
-		command = 'fa'
-	}
-	if (command == 'stream') {
-		command = 'si'
-	}
-	if (command == 'rem') {
-		command = 'listemotes'
-	}
-	if (command == 'birthday') {
-		command = 'bday'
-	}
-	if (command == 'top') {
-		command = 'topstreams'
-	}
-	if (command == 'subage') {
-		command = 'sa'
-	}
+  if(command == 'followage') { command = 'fa' }
+  if(command == 'stream') { command = 'si' }
+  if(command == 'birthday') { command = 'bday' }
+  if(command == 'top') { command = 'topstreams' }
+  if(command == 'subage') { command = 'sa' }
+	if (command == 'query') { command = 'wolfram' }
 	command = command.toLowerCase() + '.js'
 	let commandCheck = (await fs.readdir(__dirname)).indexOf(command)
 	if (commandCheck == -1 || command == 'handler.js') {
