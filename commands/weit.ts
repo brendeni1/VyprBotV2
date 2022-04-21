52d5754487f48e132da5a211f0a2d49fbe0a1f3e
Brenden <70484211+DarkVypr@users.noreply.github.com>
2022-04-21 01:39:32 +0000

    fixed weit source

--- commands/weit.ts
+++ commands/weit.ts
@@ -1,14 +1,14 @@
 import utils from '../utils'
 
 module.exports = async (client, context) => {
-	if (!context.args[0]) {
-		return {
-			success: false,
-			reply: `Please provide an emote or emote code/id to look up. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
-		}
-	}
+  if (!context.args[0]) {
+    return {
+      success: false,
+      reply: `Please provide an emote or emote code/id to look up. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
+    }
+  }
   let query = context.args[0]
-	let isEmoteID = /^\d+$/.test(query) || /emotesv2_[a-z0-9]{32}/.test(query)
+  let isEmoteID = /^\d+$/.test(query) || /emotesv2_[a-z0-9]{32}/.test(query)
   let isEmoteLink = /https:\/\/static\-cdn\.jtvnw\.net\/emoticons\/v2\/\S+\/default\/dark\/[1-5]\.0/.test(query)
   if (isEmoteLink) {
     query = query.replace('https://static-cdn.jtvnw.net/emoticons/v2/', '').replace(/\/default\/dark\/[1-5]\.0/, '')
@@ -18,12 +18,14 @@ module.exports = async (client, context) => {
     query = context.emotes[0].id
     isEmoteID = true
   }
-	try {
-		const emoteData = await utils.fetch(`https://api.ivr.fi/v2/twitch/emotes/${query}?id=${String(isEmoteID)}`)
+  try {
+    const emoteData = await utils.fetch(`https://api.ivr.fi/v2/twitch/emotes/${query}?id=${String(isEmoteID)}`)
     const sourceType = emoteData.emoteType == 'GLOBALS'
       ? 'global Twitch emote.'
       : emoteData.emoteType == 'SUBSCRIPTIONS'
+      ? emoteData.channelLogin
       ? `Tier ${emoteData.emoteTier} subscriber emote to the channel @${emoteData.channelLogin}`
+      : `Tier ${emoteData.emoteTier} subscriber emote to an unknown/deleted channel.`
       : emoteData.emoteType == 'FOLLOWER'
       ? `follower emote to the channel @${emoteData.channelLogin}`
       : emoteData.emoteType == 'SMILIES'
@@ -37,16 +39,16 @@ module.exports = async (client, context) => {
       success: true,
       reply: `${emoteData.emoteCode} (ID: ${emoteData.emoteID}) is a ${emoteData.emoteAssetType.toLowerCase()} ${sourceType} ${emoteLink} ${authorLink}`
     }
-	} catch (err) {
+  } catch (err) {
     if (err == 'Error: Not Found') {
-  		return {
-  			success: false,
-  			reply: `That emote wasn't valid! Use the emote's url instead. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
-  		}
+      return {
+        success: false,
+        reply: `That emote wasn't valid! Use the emote's url instead. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
+      }
     }
-		return {
-			success: false,
-			reply: err
-		}
-	}
+    return {
+      success: false,
+      reply: err
+    }
+  }
 }
