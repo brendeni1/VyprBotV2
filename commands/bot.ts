2e0edfbb9f99d263512308bdae4a393f18a09c3f
Brenden <70484211+DarkVypr@users.noreply.github.com>
2022-05-01 16:24:08 +0000

    cool shit

--- commands/bot.ts
+++ commands/bot.ts
@@ -7,13 +7,18 @@ module.exports = async (client, context) => {
 		if (idCheck) {
 			context.args.splice(context.args.indexOf(idCheck[0]), 1)
 		}
-		let user = context.args[0] ? context.args[0].toLowerCase().replace('@', '') : context.user
+		let user = context.args[0] 
+    ? context.args[0].toLowerCase().replace('@', '')
+    : context.user
 		const userData = await utils.fetch(`https://api.ivr.fi/v2/twitch/user/${user}?id=${id}`)
 		const adverb = userData.verifiedBot ? '' : 'not'
 		const emoji = userData.verifiedBot ? ' ✅ ' : ' ❌ '
+    const self = user == 'vyprbot'
+    ? '(w manz https://i.darkvypr.com/bot-verification.png 😎)'
+    : ''
 		return {
 			success: true,
-			reply: `${userData.displayName} is ${adverb} a verified bot! ${emoji}`
+			reply: `${userData.displayName} is ${adverb} a verified bot! ${emoji} ${self}`
 		}
 	} catch (e) {
 		return {
