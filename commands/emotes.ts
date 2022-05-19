90afc39b8f8e0cbb51de27cd043f704b3eca6f33
Brenden <70484211+DarkVypr@users.noreply.github.com>
2022-05-19 01:55:40 +0000

    error catching for emotes command

--- commands/emotes.ts
+++ commands/emotes.ts
@@ -1,16 +1,26 @@
 import utils from '../utils'
 
 module.exports = async (client, context) => {
-  const channel = context.args[0] ? context.args[0].toLowerCase().replace('@', '') : context.channel
+  const channel = context.args[0]
+    ? context.args[0].toLowerCase().replace('@', '')
+    : context.channel
   try {
-    await utils.userExists(channel)
-    return { success: true, reply: `https://emotes.raccatta.cc/twitch/${channel}` }
+    if (await utils.userExists(channel)) {
+      return {
+        success: true,
+        reply: `https://emotes.raccatta.cc/twitch/${channel}`
+      }
+    }
+    return {
+      success: false,
+      reply: `That channel wasn't found! ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
+    }
   } catch (e) {
     if (e == 'Error: Not Found') {
-  		return {
-  			success: false,
-  			reply: `That emote wasn't valid! Use the emote's url instead. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
-  		}
+      return {
+        success: false,
+        reply: `That channel wasn't found! ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', '😵', '⛔'])}`
+      }
     }
     return {
       success: false,
