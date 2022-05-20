30d14d36e701b5f7f723cdffa12e4af0fda73bba
Brenden <70484211+DarkVypr@users.noreply.github.com>
2022-05-20 19:04:19 +0000

    bot colour command fixed

--- commands/botcolour.ts
+++ commands/botcolour.ts
@@ -2,37 +2,34 @@ import utils from '../utils'
 
 module.exports = async (client, context) => {
 	const colours = ['red', 'blue', 'green', 'firebrick', 'coral', 'yellowgreen', 'orangered', 'seagreen', 'goldenrod', 'chocolate', 'cadetblue', 'dodgerblue', 'hotpink', 'blueviolet', 'springgreen']
-	if (!context.args[0] || colours.indexOf(context.args[0].toLowerCase())) {
+	let colour = context.args.join('')
+  console.log(colour.toLowerCase())
+  if (!colour || colours.indexOf(colour.toLowerCase()) == -1) {
 		return {
 			success: false,
-			reply: `Invalid colour! Colours: ${colours.join(', ')}`
+			reply: `Invalid colour! Colours: ${utils.capitalizeEachWord(colours.join(', '))} ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', 'FeelsBadMan', '😵', '⛔'])}`
 		}
 	}
+  colour = colour.toLowerCase().replace(' ', '')
 	let nammers = await utils.getData(`${context.user}Nammers`)
 	if (!nammers || +nammers == 0) {
 		return {
 			success: false,
-			reply: `You don't have any nammers! Use "${context.prefix}hunt" to get some.`
+			reply: `You don't have any nammers! Use "${context.prefix}hunt" to get some. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', 'FeelsBadMan', '😵', '⛔'])}`
 		}
 	}
 	nammers = +nammers
 	if (nammers < 200) {
 		return {
 			success: false,
-			reply: `You don't have enough nammers! You need 200 nammers, but only have ${nammers}. Use "${context.prefix}hunt" to get some more.`
+			reply: `You don't have enough nammers! You need 200 nammers, but only have ${nammers}. Use "${context.prefix}hunt" to get some more. ${await utils.bestEmote(context.channel, ['BRUHFAINT', 'BruhFaint', 'PANIC', 'FeelsDankMan', 'FeelsBadMan', '😵', '⛔'])}`
 		}
 	}
-	const win = utils.randInt(0, 1) == 0
-	if (win) {
-		utils.setData(`${context.user}Nammers`, nammers + amount)
-		return {
-			success: true,
-			reply: `You won ${amount} nammer${amount==1?'':'s'}! You now have ${nammers + amount}.`
-		}
-	}
-	utils.setData(`${context.user}Nammers`, nammers - amount)
-	return {
-		success: true,
-		reply: `You lost ${amount} nammer${amount==1?'':'s'}! You now have ${nammers - amount}.`
-	}
+  nammers = nammers - 200
+  client.privmsg(context.channel, `/color ${colour}`)
+  await utils.setData(`${context.user}Nammers`, nammers)
+  return {
+    success: true,
+    reply: `Successfully set my colour to: ${colour.toUpperCase()}. You now have ${nammers} (-200) ${await utils.bestEmote(context.channel, ['EZ', 'HYPE', 'Swagging', 'YAAAY', 'YESIDOTHINKSO', '😎', '😄'])}`
+  }
 }
